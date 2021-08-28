3ae3a603dfdf80a8c54d7e9fdb328a9cb93fc66d
Your Name <robertcjohnson1984@gmail.com>
2021-08-27 22:33:43 -0600

    contact pg init commit

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container, Row, Col } from 'react-bootstrap';
+
+const Contact = (props) => {
+    return (
+    <Container>
+        <Row>
+            <Col><img src="public/assets/img/rob/rob-1.png" class="img-fluid" alt="..."></img></Col>
+            <Col><h1>Contact</h1></Col>
+        </Row>
+    </Container>
+ )
+}
+
+export default Contact;
