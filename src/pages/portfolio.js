119604371c4e36e58b415f0f42c02fbd2ca6bc17
Your Name <robertcjohnson1984@gmail.com>
2021-08-27 22:27:04 -0600

    port page init commit

---
+++ src/pages/portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Container, Row, Col } from 'react-bootstrap';
+
+const Portfolio = (props) => {
+    return (
+    <Container>
+        <Row>
+            <Col><img src="public/assets/img/rob/rob-1.png" class="img-fluid" alt="..."></img></Col>
+            <Col><h1>Hello everyone</h1></Col>
+        </Row>
+    </Container>
+ )
+}
+
+export default Portfolio;
