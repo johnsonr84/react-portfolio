9aa5fae10ad660214f2ae208f495735279a3ee79
Your Name <robertcjohnson1984@gmail.com>
2021-08-28 17:18:48 -0600

    update img import name

--- src/pages/story.js
+++ src/pages/story.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Container, Row, Col } from 'react-bootstrap';
+import storyPNG from "../assets/images/rob/rob-6.png";
 
 const Story = (props) => {
     return (
     <Container>
         <Row>
-            <Col><img src="public/assets/img/rob/rob-1.png" class="img-fluid" alt="..."></img></Col>
+        <Col><img src={storyPNG} className="img-fluid" alt="..." /></Col>
             <Col><h1>Story</h1></Col>
         </Row>
     </Container>
