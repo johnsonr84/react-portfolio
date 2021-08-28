94f45665589594fe98864c37a96cbcff87c823a0
Your Name <robertcjohnson1984@gmail.com>
2021-08-28 16:17:16 -0600

    add img

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Container, Row, Col } from 'react-bootstrap';
+import robContact from "../assets/images/rob/rob-1.png";
 
 const Contact = (props) => {
     return (
     <Container>
         <Row>
-            <Col><img src="public/assets/img/rob/rob-1.png" class="img-fluid" alt="..."></img></Col>
+            <Col><img src={robContact} className="img-fluid" alt="..." /></Col>
             <Col><h1>Contact</h1></Col>
         </Row>
     </Container>
