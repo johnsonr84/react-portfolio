a1dd3b3b9a7b418304e9d770f4e549f46933c7f3
Your Name <robertcjohnson1984@gmail.com>
2021-08-28 16:11:53 -0600

    switch routes for pgs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Home from "./pages/home"
+import Portfolio from "./pages/portfolio"
+import Story from "./pages/story"
+import Contact from "./pages/contact"
 import Navbar from './components/Navbar'
-import { BrowserRouter as Route, Switch } from "react-router-dom";
+import {  Route, Switch } from "react-router-dom";
 import './App.css';
+// import Portfolio from "./pages/portfolio";
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/story" component={Story} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
     </Route>
   );
