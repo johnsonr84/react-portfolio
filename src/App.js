a76a4f7fd67a7e90c72d02b259c719570ea82aae
Your Name <robertcjohnson1984@gmail.com>
2021-08-27 22:15:18 -0600

    rm react boiler content add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import Home from "./pages/home"
+import Navbar from './components/Navbar'
+import { BrowserRouter as Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Route>
   );
 }
 
