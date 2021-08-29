983d8f5371b5fd8c283c2cf04d5c6d709a07eeb1
Your Name <robertcjohnson1984@gmail.com>
2021-08-29 03:32:24 -0600

    adj col size and spacing

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,23 +5,23 @@ import homePNG from "../assets/images/rob/rob-2.png";
 const Home = (props) => {
     return (
         <Container>
-            <Row className="display-flex">
-                <Col>
-                    <h1 class="main-title">Hey!</h1><small>Thanks for stopping by. </small>
+            <Row className="display-flex align-items-center mx-auto">
+                <Col sm={4}>
+                    {/* <h1 class="main-title">Hey!</h1><small>Thanks for stopping by. </small> */}
                     <p>
-                        I am Rob, a <b class="front-end">Front End</b> and <b class="word-press">WordPress Developer</b>
-                        who is passionate about building projects to help
-                        people and society. Positive energy, a flair for problem solving and making people laugh is what I
-                        am known for. I consider
-                        myself fortunate to do what I love daily.
+                        I am Rob, a <b class="front-end">Full Stack Developer </b>
+                        who is passionate about building projects to provide solutions and help
+                        others.
                     </p>
                     <p>
-                        I am currently expanding my dev skills as a student at the University of Utah <b
-                            class="full-stack">Full Stack Developer</b> Coding Bootcamp in
-                        evening Zoom classes.
+                        Positive energy, a flair for problem solving and making people laugh is what I
+                        am known for.
                     </p>
-                </Col>
-                <Col><img src={homePNG} className="img-fluid" alt="..." /></Col>
+                    <p>
+                        I consider myself fortunate to do what I love daily.
+                    </p>
+                </Col >
+                <Col sm={8}><img src={homePNG} className="img-fluid" alt="..." /></Col>
             </Row>
         </Container>
 
