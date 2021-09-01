605419a997866bd8537e0899755c95cf54ca69b2
Your Name <robertcjohnson1984@gmail.com>
2021-08-31 18:24:22 -0600

    add projects btn

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import homePNG from "../assets/images/rob/rob-5.png";
 
 const Home = (props) => {
     return (
         <Container>
-            <Row className="display-flex align-items-center mx-auto">
+            <Row className="display-flex align-items-center justify-content-center mx-auto">
                 <Col md={4}>
                     {/* <h1 class="main-title">Hey!</h1><small>Thanks for stopping by. </small> */}
                     <p>
@@ -20,6 +20,9 @@ const Home = (props) => {
                     <p>
                         I consider myself fortunate to do what I love daily.
                     </p>
+                    <div className="justify-content-center align-items-center">
+                        <Button variant="secondary" size="lg" href="#Portfolio">Projects</Button>
+                    </div>
                 </Col >
                 <Col md={8}><img src={homePNG} className="img-fluid" alt="..." /></Col>
             </Row>
