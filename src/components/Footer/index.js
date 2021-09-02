18db81cdac319e2274a909cf515c4ba0cc87a2f7
Your Name <robertcjohnson1984@gmail.com>
2021-09-01 22:46:04 -0600

    add footer components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,6 +5,19 @@ import "./style.css"
 const Footer = (props) => {
     return (
 
+<footer>
+    <Container>
+        {/* <div>
+            <ContactForm />
+        </div>
+        <div>
+          <LinkedIn />
+          <GitHub />
+        </div> */}
+    </Container>
+  </footer>
    
+        )
+    }
 
 export default Footer;
