5c388be32714982cd825118ed0b2443545df5e41
Your Name <robertcjohnson1984@gmail.com>
2021-09-10 13:21:23 -0600

    set vh-100

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,14 +5,17 @@ import homePNG from "../assets/images/rob/rob-5.png";
 const Home = (props) => {
     return (
         <Container>
-            <Row className="display-flex align-items-center justify-content-center mx-auto">
-            <Col md={8}><img src={homePNG} className="img-fluid" alt="..." /></Col>
+            <Row className="display-flex align-items-center justify-content-center mx-auto vh-100">
+                <Col md={8}><img src={homePNG} className="img-fluid" alt="..." /></Col>
                 <Col md={4}>
-                    {/* <h1 class="main-title">Hey!</h1><small>Thanks for stopping by. </small> */}
+                    <h1>
+                        Heyo! I'm Rob.
+                    </h1>
+                    <h4>
+                        A Full Stack Developer.
+                    </h4>
                     <p>
-                        Heyo, I am Rob! A <b class="front-end">Full Stack Developer </b>
-                        who is passionate about building projects to provide solutions and help
-                        others.
+                        I enjoy building projects to provide solutions and help others.
                     </p>
                     <p>
                         Positive energy, a flair for problem solving and making people laugh is what I
@@ -22,7 +25,7 @@ const Home = (props) => {
                         I consider myself fortunate to do what I love daily.
                     </p>
                     <div className="justify-content-center align-items-center">
-                        <Button variant="secondary" size="lg" href="#Portfolio">Projects</Button>
+                        <Button variant="secondary" size="md" href="#Portfolio">Projects</Button>
                     </div>
                 </Col >
             </Row>
