dde6998db1970151308872373f0e5b0c38d7c0e0
Your Name <robertcjohnson1984@gmail.com>
2021-09-19 10:17:57 -0600

    install gatsby

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom'
 import { HashRouter } from 'react-router-dom'
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 render(
