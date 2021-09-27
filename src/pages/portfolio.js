78708b04c45f115ab8d13c2adab7c04f4e2fb03f
Your Name <robertcjohnson1984@gmail.com>
2021-09-26 20:14:39 -0600

    update emp management card

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -152,17 +152,23 @@ const Portfolio = (props) => {
                 </MDBCard>
 
 
-
-                <Card style={{ width: '18rem', padding: '1rem', margin: '1rem', textAlign: "center" }}>
-                    <Card.Title>Employee Management</Card.Title>
-                    <Card.Img src={employeeManagement} />
-                    <Card.Body>
-                        <Card.Text>
+                <MDBCard style={{ maxWidth: '20rem', margin: '1rem', textAlign: "center", }}>
+                    <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                        <MDBCardImage src={employeeManagement} fluid alt='...' />
+                        <a>
+                            <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                        </a>
+                    </MDBRipple>
+                    <MDBCardBody>
+                        <MDBCardTitle>Employee Management</MDBCardTitle>
+                        <MDBCardText>
                             Employee Database Solution
-                        </Card.Text>
-                        <Button variant="secondary" href="https://github.com/johnsonr84/employee-management" target="blank">Repository</Button>
-                    </Card.Body>
-                </Card>
+                        </MDBCardText>
+                        <MDBBtn color='secondary' href='https://github.com/johnsonr84/employee-management' target="blank">Repository</MDBBtn>
+                    </MDBCardBody>
+                </MDBCard>
+
+              
 
                 <Card style={{ width: '18rem', padding: '1rem', margin: '1rem', textAlign: "center" }}>
                     <Card.Title>Day Planner</Card.Title>
