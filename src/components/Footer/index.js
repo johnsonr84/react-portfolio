d98c55200abaf855c47bd93c29df708f2275e215
Your Name <robertcjohnson1984@gmail.com>
2021-09-26 22:34:30 -0600

    add copy right to footer and mdb

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Nav, Row } from 'react-bootstrap';
+import { MDBFooter, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
 import reactPNG from "./../../../src/assets/images/logos/react.png";
 import bootstrapPNG from "./../../../src/assets/images/logos/bootstrap.png";
 import htmlPNG from "./../../../src/assets/images/logos/html.png";
@@ -12,21 +13,32 @@ import gatsbyPNG from "./../../../src/assets/images/logos/gatsby.png";
 const Footer = (props) => {
     return (
 
-        <Container >
-            <Nav className="justify-content-center m-5">
-                <Row className="text-center">
-                    <ul>
-                        <p className="text-center">Designed by yours truly with</p>
-                        <img src={reactPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="react logo" />
-                        <img src={bootstrapPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="bootstrap logo" />
-                        <img src={htmlPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="html shield logo" />
-                        <img src={cssPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="css shield logo" />
-                        <img src={jsPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="javascript shield logo" />
-                        <img src={gatsbyPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="gatsby shield logo" />
-                        <img src={nodePNG} width="40" height="45" className="d-inline-block align-top p-1" alt="node shield logo" />
-                    </ul>
-                </Row>
-            </Nav >
+        <Container fluid className="p-0">
+          
+                <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
+                    <MDBContainer className=''>
+                        <MDBRow className="text-center">
+                            <ul>
+                                <p className="text-center">designed by yours truly with</p>
+                                <img src={reactPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="react logo" />
+                                <img src={bootstrapPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="bootstrap logo" />
+                                <img src={htmlPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="html shield logo" />
+                                <img src={cssPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="css shield logo" />
+                                <img src={jsPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="javascript shield logo" />
+                                <img src={gatsbyPNG} width="40" height="45" className="d-inline-block align-top p-1" alt="gatsby shield logo" />
+                                <img src={nodePNG} width="40" height="45" className="d-inline-block align-top p-1" alt="node shield logo" />
+                            </ul>
+                        </MDBRow>
+                    </MDBContainer>
+                    <div className='text-center mt-5 mb-3' style={{ backgroundColor: '' }}>
+                        &copy; {new Date().getFullYear()} Copyright:{' '}
+                        <a className='text-dark' href='https://devdork.com/' style={{ textDecoration: 'none' }}>
+                            devdork.com
+                        </a>
+                    </div>
+                </MDBFooter>
+
+           
         </Container>
 
     )
