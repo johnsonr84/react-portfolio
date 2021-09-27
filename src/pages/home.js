7068f62e100785474824db380733ba17ecfc57a8
Your Name <robertcjohnson1984@gmail.com>
2021-09-26 21:27:54 -0600

    fix spacing in opener

--- src/pages/home.js
+++ src/pages/home.js
@@ -14,10 +14,10 @@ const Home = (props) => {
                     <h1>
                         Hey! I'm Rob.
                     </h1>
-                    <h4>
+                    <h4 className='pt-2'>
                         A Full Stack Developer
                     </h4>
-                    <p>
+                    <p className='pt-2'>
                         I enjoy building projects to provide solutions and help others.
                     </p>
                     <p>
@@ -27,19 +27,22 @@ const Home = (props) => {
                     <p>
                         I consider myself fortunate to do what I love daily.
                     </p>
-                    <div>
-                        <MDBBtn className='mx-3 hover-shadow-strong' style={{ backgroundColor: '#5B6369', width: '100px', border: 'none' }}  href="#Portfolio">
+                    <p>
+                        Currently, I am seeking work as a Full Stack Developer, while doing online classes with Harvard Extension School in Computer Science.
+                    </p>
+                    <div className='pt-2'>
+                        <MDBBtn className='mx-3 mt-1 hover-shadow-strong' style={{ backgroundColor: '#5B6369', width: '100px', border: 'none' }} href="#Portfolio">
                             {/* <MDBIcon fas icon='code-branch' />  */}
                             Projects
                         </MDBBtn>
-                        <MDBBtn className='mx-1 hover-shadow-strong rounded-circle' style={{ backgroundColor: '#333333', border: 'none' }}  href='https://github.com/johnsonr84' target='blank'>
-                            <MDBIcon fab icon='github' /> 
+                        <MDBBtn className='mx-1 mt-1 hover-shadow-strong rounded-circle' style={{ backgroundColor: '#333333', border: 'none' }} href='https://github.com/johnsonr84' target='blank'>
+                            <MDBIcon fab icon='github' />
                         </MDBBtn>
-                        <MDBBtn className='mx-1 hover-shadow-strong rounded-circle' style={{ backgroundColor: '#0670AE', border: 'none' }}  href='https://www.linkedin.com/in/robertjohnson1984/' target='blank'>
-                            <MDBIcon fab icon='linkedin-in' /> 
+                        <MDBBtn className='mx-1 mt-1 hover-shadow-strong rounded-circle' style={{ backgroundColor: '#0670AE', border: 'none' }} href='https://www.linkedin.com/in/robertjohnson1984/' target='blank'>
+                            <MDBIcon fab icon='linkedin-in' />
                         </MDBBtn>
-                        <MDBBtn  className='mx-1 hover-shadow-strong hover-zoom rounded-circle' style={{ backgroundColor: '#dd4b39', border: 'none' }}  href='#'>
-                            <MDBIcon fab icon='google' /> 
+                        <MDBBtn className='mx-1 mt-1 hover-shadow-strong hover-zoom rounded-circle' style={{ backgroundColor: '#dd4b39', border: 'none' }} href='#'>
+                            <MDBIcon fab icon='google' />
                         </MDBBtn>
                     </div>
 
