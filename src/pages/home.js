ee2e9e01edf3f1ebcc4331e3a87fa1b05cb1a897
Your Name <robertcjohnson1984@gmail.com>
2021-09-28 23:39:00 -0600

    update w/ MDB

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import homePNG from "../assets/images/rob/rob-5.png";
 import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
@@ -7,10 +8,10 @@ import { MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 
 const Home = (props) => {
     return (
-        <Container>
-            <Row className="display-flex align-items-center justify-content-center mx-auto vh-100">
-                <Col md={8}><img src={homePNG} className="img-fluid" alt="..." /></Col>
-                <Col md={4}>
+        <MDBContainer>
+            <MDBRow className="display-flex align-items-center justify-content-center mx-auto vh-100">
+                <MDBCol md={8}><img src={homePNG} className="img-fluid" alt="..." /></MDBCol>
+                <MDBCol md={4}>
                     <h1>
                         Hey! I'm Rob.
                     </h1>
@@ -31,11 +32,11 @@ const Home = (props) => {
                         Currently, I am seeking work as a Full Stack Developer, while doing online classes with Harvard Extension School in Computer Science.
                     </p>
                     <div className='pt-2'>
-                        <MDBBtn className='mx-3 mt-1 hover-shadow-strong' style={{ backgroundColor: '#5B6369', width: '100px', border: 'none' }} href="#Portfolio">
+                        <MDBBtn size='lg' className='mt-1 p-3 hover-shadow-strong' color='dark' style={{ border: 'none' }} href="#Portfolio">
                             {/* <MDBIcon fas icon='code-branch' />  */}
                             Projects
                         </MDBBtn>
-                        <MDBBtn className='mx-1 mt-1 hover-shadow-strong rounded-circle hover-color-green' style={{ backgroundColor: '#333333', border: 'none', hover: 'green' }} href='https://github.com/johnsonr84' target='blank'>
+                        {/* <MDBBtn className='mx-1 mt-1 hover-shadow-strong rounded-circle hover-color-green' style={{ backgroundColor: '#333333', border: 'none', hover: 'green' }} href='https://github.com/johnsonr84' target='blank'>
                             <MDBIcon fab icon='github' />
                         </MDBBtn>
                         <MDBBtn className='mx-1 mt-1 hover-shadow-strong rounded-circle' style={{ backgroundColor: '#0670AE', border: 'none' }} href='https://www.linkedin.com/in/robertjohnson1984/' target='blank'>
@@ -43,34 +44,29 @@ const Home = (props) => {
                         </MDBBtn>
                         <MDBBtn className='mx-1 mt-1 hover-shadow-strong hover-zoom rounded-circle' style={{ backgroundColor: '#dd4b39', border: 'none' }} href='#'>
                             <MDBIcon fab icon='google' />
-                        </MDBBtn>
+                        </MDBBtn> */}
                     </div>
 
-                </Col >
-            </Row>
-            {/* <Row className="display-flex align-items-center text-center">
-                <Col md={2} className="display-flex align-items-center">
-                    <MDBBtn rounded className='mx-1 shadow' style={{ backgroundColor: '#0082ca' }} floating size='lg' href='https://www.linkedin.com/in/robertjohnson1984/' target='blank'>
+                </MDBCol >
+            </MDBRow>
+            {/* <MDBRow fluid className="align-items-center text-center" style={{ backgroundColor: 'grey', height: '300px' }}>
+                <MDBCol size='4' className="display-flex align-items-center">
+                   
+                        <MDBIcon size='8x' fab icon="github" ><a href='https://www.linkedin.com/in/robertjohnson1984/' target='blank' ></a></MDBIcon>
+                   
+                </MDBCol>
+                <MDBCol size='4' className="display-flex align-items-center text-center">
+                    <MDBBtn className='shadow' style={{ backgroundColor: '#333333' }}  href='https://github.com/johnsonr84' target='blank'>
+                    <MDBIcon size='8x' fab icon="github" />
+                    </MDBBtn>
+                </MDBCol>
+                <MDBCol size='4' className="display-flex align-items-center text-center" >
+                    <MDBBtn className='shadow' style={{ backgroundColor: '#0082ca' }} floating size='lg' href='https://www.linkedin.com/in/robertjohnson1984/' target='blank'>
                         <MDBIcon fab icon='linkedin-in' />
                     </MDBBtn>
-                </Col>
-                <Col md={2} className="display-flex align-items-center">
-                    <MDBBtn className='m-1 shadow' style={{ backgroundColor: '#333333' }} floating size='lg' href='https://github.com/johnsonr84' target='blank'>
-                        <MDBIcon fab icon='github' />
-                    </MDBBtn>
-                </Col>
-                <Col md={2} className="display-flex align-items-center" >
-                    <MDBBtn className='mx-1 shadow' style={{ backgroundColor: '#0082ca' }} floating size='lg' href='https://www.linkedin.com/in/robertjohnson1984/' target='blank'>
-                        <MDBIcon fab icon='linkedin-in' />
-                    </MDBBtn>
-                </Col>
-
-
-
-                <Col md={4}><a className="fab fa-github" href="https://github.com/johnsonr84?tab=repositories" target="blank" /></Col>
-                <a target="blank">Repositories</a>
-            </Row> */}
-        </Container>
+                </MDBCol>
+            </MDBRow> */}
+        </MDBContainer>
 
     )
 }
