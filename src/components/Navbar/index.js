52408d610742065523177cd68054cba7ffbebeac
Your Name <robertcjohnson1984@gmail.com>
2021-09-29 21:50:17 -0600

    add mdb nav components

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,44 +1,47 @@
 import React from 'react'
 import { Navbar, Container, Nav, Row, Col, NavDropdown } from 'react-bootstrap';
+import {
+    MDBNavbar,
+    MDBContainer,
+    MDBNavbarBrand,
+} from 'mdb-react-ui-kit';
 import "./style.css"
 
 const CustomNavbar = (props) => {
     return (
+        <MDBNavbar sticky light bgColor='light'>
+            <MDBContainer fluid>
 
-        <Container fluid className="">
-            <Row>
-                <Navbar className="navbar">
+                <Col>
+                    <Nav>
+                        <ul>
+                            <li>
+                                <MDBNavbarBrand href='#'>Rob C Johnson</MDBNavbarBrand>
+                            </li>
+                        </ul>
+                    </Nav>
+                </Col>
 
-                    <Col>
-                        <Nav>
-                            <ul>
-                                <li>
-                                    <Navbar.Brand href="#">Rob C Johnson</Navbar.Brand>
-                                </li>
-                            </ul>
-                        </Nav>
-                    </Col>
-                   
-                    <Col xs={6}>
-                        <Nav className="pages justify-content-center p-2">
-                            <Nav.Item>
-                                <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href="#Story">Story</Nav.Link>
-                            </Nav.Item>
-                            {/* <Nav.Item>
+                <Col xs={6}>
+                    <Nav className="pages justify-content-center p-2">
+                        <Nav.Item>
+                            <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#Story">Story</Nav.Link>
+                        </Nav.Item>
+                        {/* <Nav.Item>
                                 <Nav.Link href="#Contact">Contact</Nav.Link>
                             </Nav.Item> */}
-                            <Nav.Item>
-                                <Nav.Link href="https://www.visualcv.com/robert-johnson/" target="none">Résumé</Nav.Link>
-                            </Nav.Item>
-                        </Nav>
-                    </Col>
-                   
-                    <Col>
-                        <Nav className="justify-content-end pb-2">
-                            <NavDropdown title="Menu">
+                        <Nav.Item>
+                            <Nav.Link href="https://www.visualcv.com/robert-johnson/" target="none">Résumé</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Col>
+
+                <Col>
+                    <Nav className="justify-content-end pb-2">
+                        <NavDropdown title="Menu">
                             <NavDropdown.Item href="#">DevDork</NavDropdown.Item>
                             <NavDropdown.Item href="#Portfolio">Portfolio</NavDropdown.Item>
                             <NavDropdown.Item href="#Story">Story</NavDropdown.Item>
@@ -46,14 +49,11 @@ const CustomNavbar = (props) => {
                             <NavDropdown.Item href="https://www.visualcv.com/robert-johnson/" target="none">Resume</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="https://github.com/johnsonr84" target="none">Github</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Col>
-
-                </Navbar >
-            </Row>
-        </Container>
-
+                        </NavDropdown>
+                    </Nav>
+                </Col>
+            </MDBContainer>
+        </MDBNavbar>
 
     )
 }
