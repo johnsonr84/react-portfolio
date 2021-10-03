afde88860d2a6886f5eee680d3498d1c6a5db850
Your Name <robertcjohnson1984@gmail.com>
2021-10-02 22:53:24 -0600

    rm icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,23 +9,23 @@ const Footer = (props) => {
         <Container fluid className="pt-5 pb-2 mt-5" style={{ boxShadow: '20px 20px 20px 20px rgb(0 0 0 / 5%)' }}>
                 <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
                     <MDBContainer className=''>
-                        <MDBRow className="text-center pt-3 pb-3">
+                        <MDBRow className="text-center pt-3">
                             <ul>
-                                <p className="text-center" style={{ fontSize: '12px'}} >Designed by yours truly with</p>
-                                <MDBIcon className='ms-1' fab icon='react' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='js' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='bootstrap' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='mdb' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='html5' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='css3-alt' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='sass' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='npm' size='lg' />
+                                <p className="text-center" style={{ fontSize: '12px'}} >Designed by yours truly with<MDBIcon className='ms-1' fab icon='react' size='lg' /></p>
+                                {/* <MDBIcon className='ms-1' fab icon='react' size='2x' />
+                                <MDBIcon className='ms-1' fab icon='js' size='2x' /> */}
+                                {/* <MDBIcon className='ms-1' fab icon='bootstrap' size='lg' /> */}
+                                {/* <MDBIcon className='ms-1' fab icon='mdb' size='2x' />
+                                <MDBIcon className='ms-1' fab icon='html5' size='2x' />
+                                <MDBIcon className='ms-1' fab icon='css3-alt' size='2x' />
+                                <MDBIcon className='ms-1' fab icon='apple' size='2x' /> */}
+                                {/* <MDBIcon className='ms-1' fab icon='npm' size='lg' />
                                 <MDBIcon className='ms-1' fab icon='git' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='node-js' size='lg' />
+                                <MDBIcon className='ms-1' fab icon='node-js' size='lg' /> */}
                             </ul>
                         </MDBRow>
                     </MDBContainer>
-                    <div className='text-center mt-1 mb-3' style={{ fontSize: '12px'}} >
+                    <div className='text-center  mb-3' style={{ fontSize: '12px'}} >
                         &copy; {new Date().getFullYear()} copyright:{' '}
                         <a className='text-dark' href='https://devdork.com/' style={{ textDecoration: 'none', fontSize: '12px'}}>
                             devdork.com
