c148dda8051490a880b5a72fffd06446d5aec88d
Your Name <robertcjohnson1984@gmail.com>
2021-10-03 17:13:49 -0600

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,33 +6,21 @@ import { MDBIcon } from 'mdb-react-ui-kit';
 const Footer = (props) => {
     return (
 
-       
-                <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
-                    <MDBContainer fluid className='pt-5 mt-5'>
-                        <MDBRow className="text-center pt-3">
-                            <ul>
-                                <p className="text-center" style={{ fontSize: '12px'}} >Designed by yours truly with<MDBIcon className='ms-1' fab icon='react' size='lg' /></p>
-                                {/* <MDBIcon className='ms-1' fab icon='react' size='2x' />
-                                <MDBIcon className='ms-1' fab icon='js' size='2x' /> */}
-                                {/* <MDBIcon className='ms-1' fab icon='bootstrap' size='lg' /> */}
-                                {/* <MDBIcon className='ms-1' fab icon='mdb' size='2x' />
-                                <MDBIcon className='ms-1' fab icon='html5' size='2x' />
-                                <MDBIcon className='ms-1' fab icon='css3-alt' size='2x' />
-                                <MDBIcon className='ms-1' fab icon='apple' size='2x' /> */}
-                                {/* <MDBIcon className='ms-1' fab icon='npm' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='git' size='lg' />
-                                <MDBIcon className='ms-1' fab icon='node-js' size='lg' /> */}
-                            </ul>
-                        </MDBRow>
-                    </MDBContainer>
-                    <div className='text-center  mb-3' style={{ fontSize: '12px'}} >
-                        &copy; {new Date().getFullYear()} copyright:{' '}
-                        <a className='text-dark' href='https://devdork.com/' style={{ textDecoration: 'none', fontSize: '12px'}}>
-                            devdork.com
-                        </a>
-                    </div>
-                </MDBFooter>
-      
+
+        <MDBFooter backgroundColor='light' className='text-center text-lg-left'>
+            <MDBContainer fluid className='pt-5 mt-5'>
+                <MDBRow className="text-center pt-3">
+                    <p className="text-center" style={{ fontSize: '12px' }} >Designed by yours truly with<MDBIcon className='ms-1' fab icon='react' size='lg' /></p>
+                </MDBRow>
+            </MDBContainer>
+            <div className='text-center  mb-3' style={{ fontSize: '12px' }} >
+                &copy; {new Date().getFullYear()} copyright:{' '}
+                <a className='text-dark' href='https://devdork.com/' style={{ textDecoration: 'none', fontSize: '12px' }}>
+                    devdork.com
+                </a>
+            </div>
+        </MDBFooter>
+
 
     )
 }
