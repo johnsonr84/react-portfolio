59f18bd9ee130541acd3433454c644ea234d960a
Your Name <robertcjohnson1984@gmail.com>
2021-10-10 19:19:05 -0600

    add contact route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './index.css';
 import Home from "./pages/home"
 import Portfolio from "./pages/portfolio"
 import Story from "./pages/story"
-// import Contact from "./pages/contact"
+import Contact from "./pages/contact"
 import Navbar from './components/Navbar'
 import Nav from './components/Footer'
 import { Route, Switch } from "react-router-dom";
@@ -18,7 +18,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/story" component={Story} />
-        {/* <Route exact path="/contact" component={Contact} /> */}
+        <Route exact path="/contact" component={Contact} />
       </Switch>
       <Nav />
     </Route>
