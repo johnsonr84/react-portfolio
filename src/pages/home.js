32e527ffac57b56f0ceb153cbead50606f4832ad
Your Name <robertcjohnson1984@gmail.com>
2021-10-10 19:47:22 -0600

    fix card

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardImage } from 'mdb-react-ui-kit';
 import homePNG from "../assets/images/rob/rob-5.png";
 import laptop from "../assets/images/devices/laptop.png"
 
 const Home = (props) => {
     return (
-        <MDBContainer>
-            <MDBRow className="display-flex align-items-center justify-content-center mx-auto vh-100">
-                <MDBCol size='8'><img src={homePNG} className="img-fluid" alt="..." /></MDBCol>
-                <MDBCol size='4'>
+        <MDBContainer >
+            <MDBRow className="display-flex align-items-center justify-content-center mx-auto vh-100" >
+                <MDBCol lg={8}><img src={homePNG} className="img-fluid" alt="..." /></MDBCol>
+                <MDBCol lg={4}>
                     <h1>
                         Hey! I'm Rob.
                     </h1>
@@ -36,13 +36,13 @@ const Home = (props) => {
                     </div>
                 </MDBCol >
             </MDBRow>
-            <MDBRow center className="align-items-center" style={{ height: '800px', paddingTop: '200px', paddingBottom: '200px', paddingLeft: '100px' }}>
-                <MDBCol size='6' className="justify-content-end text-end">
-                    <MDBCard fluid className='shadow-2-strong' style={{   padding: '2rem', textAlign: "center", }}>
-                        <MDBCardBody>I have demonstrated working knowledge of Bootstrap, CSS, Express.js, Git, Github, Heroku, HTML (HTML5), JavaScript, JQuery, MongoDB, MySQL, Node.js, React, SEO, Responsive Design, API/JSON, RESTful API, State Management, Command Line, Database Theory, Agile Methodologies, Quality Assurance Testing, Social Coding Best Practices, Computer Science Fundamentals.</MDBCardBody>
+            
+            <MDBRow center className="position-relative align-items-center justify-content-center mx-auto vh-100" style={{ height: '800px', paddingTop: '200px', paddingBottom: '200px' }}>
+                    <MDBCard fluid className='shadow-2-strong' style={{   padding: '2rem', textAlign: "center", zIndex: '2'}}>
+                        <MDBCardBody>I have demonstrated working knowledge of Bootstrap, CSS, Express.js, Git, Github, Heroku, HTML (HTML5), JavaScript, JQuery, MongoDB, MySQL, Node.js, React, SEO, Responsive Design, API/JSON, RESTful API, State Management, Command Line, Database Theory, Agile Methodologies, Quality Assurance Testing, Social Coding Best Practices, Computer Science Fundamentals.</MDBCardBody>       
+                       
                     </MDBCard>
-                </MDBCol >
-                <MDBCol size='6'><img src={laptop} className="img-fluid" alt="..." /></MDBCol>
+                    {/* <img src={laptop} className="img-fluid" alt="..." style={{ paddingLeftt: '400px', zIndex: '1' }}/> */}
             </MDBRow>
         </MDBContainer>
 
