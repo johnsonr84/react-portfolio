d5aa85578fa8249aef8af2945e733c8449b5eac2
Your Name <robertcjohnson1984@gmail.com>
2021-10-13 23:27:01 -0600

    add cards

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardImage } from 'mdb-react-ui-kit';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBCardTitle, MDBIcon  } from 'mdbreact';
 import homePNG from "../assets/images/rob/rob-5.png";
-import laptop from "../assets/images/devices/laptop.png"
+// import laptop from "../assets/images/devices/laptop.png"
 
 const Home = (props) => {
     return (
@@ -36,6 +36,68 @@ const Home = (props) => {
                     </div>
                 </MDBCol >
             </MDBRow>
+
+            <MDBRow>
+      <MDBCol md='4'>
+        <MDBCard
+          className='card-image'
+          style={{
+            backgroundImage:
+              "url('https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg')"
+          }}
+        >
+          <div className='text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
+            <div>
+              <h5 className='pink-text'>
+                <MDBIcon icon='chart-pie' /> Marketing
+              </h5>
+              <MDBCardTitle tag='h3' className='pt-2'>
+                <strong>This is card title</strong>
+              </MDBCardTitle>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit
+                officia accusamus minus error nisi architecto nulla ipsum
+                dignissimos. Odit sed qui, dolorum!
+              </p>
+              <MDBBtn color='pink'>
+                <MDBIcon icon='clone left' /> View project
+              </MDBBtn>
+            </div>
+          </div>
+        </MDBCard>
+      </MDBCol>
+
+      <MDBCol md='4'>
+        <MDBCard
+          className='card-image'
+          style={{
+            backgroundImage:
+              "url('https://mdbootstrap.com/img/Photos/Horizontal/City/6-col/img%20(47).jpg')"
+          }}
+        >
+          <div className='text-white text-center d-flex align-items-center rgba-indigo-strong py-5 px-4'>
+            <div>
+              <h5 className='orange-text'>
+                <MDBIcon icon='desktop' /> Software
+              </h5>
+              <MDBCardTitle tag='h3' className='pt-2'>
+                <strong>This is card title</strong>
+              </MDBCardTitle>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit
+                officia accusamus minus error nisi architecto nulla ipsum
+                dignissimos. Odit sed qui, dolorum!
+              </p>
+              <MDBBtn color='deep-orange'>
+                <MDBIcon icon='clone left' /> View project
+              </MDBBtn>
+            </div>
+          </div>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
             
             <MDBRow center className="position-relative align-items-center justify-content-center mx-auto vh-100" style={{ height: '800px', paddingTop: '200px', paddingBottom: '200px' }}>
                     <MDBCard fluid className='shadow-2-strong' style={{   padding: '2rem', textAlign: "center", zIndex: '2'}}>
