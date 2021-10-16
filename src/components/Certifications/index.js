6adbfa9d5b8494a8e62fba7a9cb5ffacf104a733
Your Name <robertcjohnson1984@gmail.com>
2021-10-15 21:16:46 -0600

    style cards

--- src/components/Certifications/index.js
+++ src/components/Certifications/index.js
@@ -6,77 +6,80 @@ import tiyPNG from "../../../src/assets/images/certifications/tiyCert.png";
 import uofuPNG from "../../../src/assets/images/certifications/uofuCert.png";
 
 const Certifications = () => {
-  return (
-    <MDBContainer>
-    <MDBRow evenly className='d-flex justify-content-center'>
-      <MDBCol lg={5}>
-        <MDBCard
-          className='card-image p-4 m-2'
-          style={{
-            backgroundImage:
-            `url(${uofuPNG})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
-          }}
-        >
-          <div className='text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
-            <div>
-              <h5 className='pink-text'>
-                <MDBIcon icon='chart-pie' /> Marketing
-              </h5>
-              <MDBCardTitle tag='h3' className='pt-2'>
-                <strong>This is card title</strong>
-              </MDBCardTitle>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit
-                officia accusamus minus error nisi architecto nulla ipsum
-                dignissimos. Odit sed qui, dolorum!
-              </p>
-              <MDBBtn color='pink'>
-                <MDBIcon icon='clone left' /> View project
-              </MDBBtn>
-            </div>
-          </div>
-        </MDBCard>
-      </MDBCol>
+    return (
+        <MDBContainer>
+            <MDBRow evenly className='d-flex justify-content-center'>
+                <MDBCol lg={5}>
+                    <MDBCard
+                        className='card-image p-1 m-2'
+                        style={{
+                            backgroundImage:
+                                `url(${uofuPNG})`,
+                            backgroundPosition: 'center',
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                            minHeight: '400px',
+                        }}
+                    >
+                        <div className='text-dark text-center d-flex align-items-center rgba-black-strong py-5 px-4'>
+                            <div>
+                                <h5 className='pink-text'>
+                                    <MDBIcon fas icon='certificate' /> Certificate
+                                </h5>
+                                <MDBCardTitle tag='h3' className='pt-2'>
+                                    <strong>Full Stack Development</strong>
+                                </MDBCardTitle>
+                                <p>
+                                    Demonstrated working knowledge of Bootstrap, CSS, Express.js,
+                                    Git, Github, Heroku, HTML (HTML5), JavaScript, JQuery, MongoDB, MySQL,
+                                    Node.js, React, SEO, Responsive Design, API/JSON, RESTful API, State Management,
+                                    Command Line, Database Theory, Agile Methodologies, Quality Assurance Testing,
+                                    Social Coding Best Practices, Computer Science Fundamentals.
+                                </p>
+                                {/* <MDBBtn color='pink'>
+                                    <MDBIcon icon='clone left' /> View project
+                                </MDBBtn> */}
+                            </div>
+                        </div>
+                    </MDBCard>
+                </MDBCol>
 
-      <MDBCol lg={5}>
-        <MDBCard
-          className='card-image p-4 m-2'
-          style={{
-            backgroundImage:
-            `url(${tiyPNG})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
-          }}
-        >
-          <div className='text-white text-center d-flex align-items-center rgba-indigo-strong py-5 px-4'>
-            <div>
-              <h5 className='orange-text'>
-                <MDBIcon icon='desktop' /> Software
-              </h5>
-              <MDBCardTitle tag='h3' className='pt-2'>
-                <strong>This is card title</strong>
-              </MDBCardTitle>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Repellat fugiat, laboriosam, voluptatem, optio vero odio nam sit
-                officia accusamus minus error nisi architecto nulla ipsum
-                dignissimos. Odit sed qui, dolorum!
-              </p>
-              <MDBBtn color='deep-orange'>
-                <MDBIcon icon='clone left' /> View project
-              </MDBBtn>
-            </div>
-          </div>
-        </MDBCard>
-      </MDBCol>
-    </MDBRow>
-    </MDBContainer>
-  )
+                <MDBCol lg={5}>
+                    <MDBCard
+                        className='card-image p-1 m-2'
+                        style={{
+                            backgroundImage:
+                                `url(${tiyPNG})`,
+                            backgroundPosition: 'center',
+                            backgroundSize: 'cover',
+                            backgroundRepeat: 'no-repeat',
+                            minHeight: '400px',
+                        }}
+                    >
+                        <div className='text-dark text-center d-flex align-items-center rgba-indigo-strong py-5 px-4'>
+                            <div>
+                                <h5 className='orange-text'>
+                                <MDBIcon fas icon='certificate' /> Certificate
+                                </h5>
+                                <MDBCardTitle tag='h3' className='pt-2'>
+                                    <strong>Front End Engineering</strong>
+                                </MDBCardTitle>
+                                <p>
+                                    Demonstrated working knowledge of CSS, Git, Github, Heroku, HTML (HTML5), 
+                                    JavaScript, JQuery, React, SEO, Responsive Design, API/JSON, RESTful API, State Management,
+                                    Command Line, Agile Methodologies, Quality Assurance Testing,
+                                    Social Coding Best Practices.
+                                </p>
+                                {/* <MDBBtn color='deep-orange'>
+                                    <MDBIcon icon='clone left' /> View project
+                                </MDBBtn> */}
+                            </div>
+                        </div>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
+    )
 }
 
 export default Certifications;
