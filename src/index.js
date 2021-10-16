bc872c0ec6b1056a6f766b2de2669dd1610356e3
Your Name <robertcjohnson1984@gmail.com>
2021-10-15 20:37:43 -0600

    add imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom'
 import { HashRouter } from 'react-router-dom'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import App from './App';
 
 render(
